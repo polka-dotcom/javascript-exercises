2367f935c353287414e9dde871399db432069c23
polka-dotcom <navyaonline7@gmail.com>
2024-06-24 20:25:46 +0400

    working solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,32 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, num) => {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1); 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) return 1;
+  let fact = 1; 
+  for (let i = num; i > 0; i--) {
+    fact *= i; 
+  } 
+  return fact;
 };
 
 // Do not edit below this line
