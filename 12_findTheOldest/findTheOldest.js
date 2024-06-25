b26c4f2ab8851f3b60f271c637edd71166861890
polka-dotcom <navyaonline7@gmail.com>
2024-06-25 18:45:46 +0400

    working solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear(); 
 
+    return people.reduce((oldest, person) => {
+        const ageOfOldest = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        const ageOfPerson = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+
+        return ageOfPerson > ageOfOldest ? person : oldest; 
+    });
 };
 
 // Do not edit below this line
