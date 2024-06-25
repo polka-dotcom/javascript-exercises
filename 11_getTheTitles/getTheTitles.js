0b12ce9734e41bd453fb6406cb2ea4a73dc23f97
polka-dotcom <navyaonline7@gmail.com>
2024-06-25 13:04:48 +0400

    working solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((info, i) => {
+        info.push(i.title);
+        return info;  
+    }, []);
 };
 
 // Do not edit below this line
