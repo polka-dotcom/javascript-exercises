95a3a0a79eaa49e4cce749f1192bf5e1be238202
polka-dotcom <navyaonline7@gmail.com>
2024-06-25 12:55:17 +0400

    working solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    num = parseInt(num)
 
+    if (isNaN(num) || num < 0) return "OOPS";
+    if (num == 0) return 0; 
+
+    let num1 = 0;
+    let num2 = 1;
+
+    for(let i = 2; i <= num; i++) {
+        let total = num1 + num2;
+        num1 = num2; 
+        num2 = total;
+    }
+    return num2;
 };
 
 // Do not edit below this line
