b0f42ce3f6e89324a4d55736295684736a3a1d81
polka-dotcom <navyaonline7@gmail.com>
2024-06-25 10:54:59 +0400

    working solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (arr) {
+    const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890"; 
+    arr = arr.toLowerCase();
+    const newArr = arr.split(''); 
 
+    const filteredArr = newArr.filter((chara) => {
+        return alphanumerical.includes(chara); 
+    });
+
+    const JfilteredArr = filteredArr.join(''); 
+
+    const reversedArr = JfilteredArr.split('').reverse().join(''); 
+
+    return reversedArr === JfilteredArr; 
 };
 
 // Do not edit below this line
